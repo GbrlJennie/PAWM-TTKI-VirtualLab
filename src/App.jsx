9ba1b1c741add7d9dcd19d8b1641f330430d9979
Naila Selvira Budiana <18223018@std.stei.itb.ac.id>
2025-10-25 13:02:18 +0700

    feat : update route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import TataKata from './pages/TataKata'
-import Login from './pages/Login';
-import KuisEjaan from './pages/KuisEjaan1';
+import Ejaan from './pages/Ejaan'
+import Login from './pages/Login'
+import KuisEjaan from './pages/KuisEjaan1'
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/Ejaan" element={<Ejaan />} />
         <Route path="/Tata-Kata" element={<TataKata />} />
         <Route path="/Tata-Kalimat" element={<TataKata />} />
